c9d721a79b8aedbda99945b76d15833796e152c7
TrueBoxGuy <gamer.tmt@gmail.com>
2020-04-06 17:11:27 +0100

    Introduce left associativity

--- src/Transpiler/Parser.hs
+++ src/Transpiler/Parser.hs
@@ -14,6 +14,7 @@ import Text.Megaparsec
 import Text.Megaparsec.Char (letterChar, string)
 import Parsers
 import Transpiler.Conversion
+import Data.List
 
 -- | Parses a term, which must consist of at least one @'letterChar'@. This parser is able to backtrack.
 term :: Parser String
@@ -24,9 +25,9 @@ term = try (some letterChar)
 lhs :: Parser LHS
 lhs = symbol $ LHS <$> symbol term <*> many (symbol term) -- sepBy is too greedy
 
--- | Parses the right hand side of an equation, which can either be a term, or an application of two '@rhs'.
+-- | Parses the right hand side of an equation, which can either be a term, or an application of two '@rhs'. Application is left associative.
 rhs :: Parser RHS
-rhs = symbol $ choice [try $ App <$> symbol part <*> part, Term <$> term]
+rhs = symbol $ choice [try $ foldl' App <$> part <*> some (symbol part), Term <$> term]
   where
     part = Term <$> term <|> brackets rhs
 
